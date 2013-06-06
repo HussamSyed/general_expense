f55d7a81f29f67a18975022d6165cd3cdc9a082e
HussamSyed <hussamusyed@gmail.com>
2013-06-06 13:21:21 -0700

    made some changes and fixed error with vehicle expenses

--- app/models/vehicle_expense.rb
+++ app/models/vehicle_expense.rb
@@ -1,11 +1,11 @@
 class VehicleExpense < ActiveRecord::Base
   attr_accessible :amount, :date, :details, :method_of_payment, :purpose, :seller, 
-  :vehicle_id, :vehicle_model, :reimbursable
+  :vehicle_id, :reimbursable
   
   #Associations
   belongs_to :vehicle
 
   #Validations
   validates_presence_of :amount, :date, :details, :method_of_payment, :purpose, 
-  :seller, :vehicle_model
+  :seller, :vehicle_id
 end
