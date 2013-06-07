5a201e0a954d7573dc196bfa6fb581d64a42ceea
mxe354 <ahmedtsyed@gmail.com>
2013-06-07 15:14:08 -0700

    removed migration causing error

--- db/migrate/20130607212734_remove_paperclip_fields_from_genexps.rb
+++
@@ -1,8 +0,0 @@
-class RemovePaperclipFieldsFromGenexps < ActiveRecord::Migration
-  def change
-    remove_column :gen_exps, :receipt_file_name
-    remove_column :gen_exps, :receipt_content_type
-    remove_column :gen_exps, :receipt_file_size
-    remove_column :gen_exps, :receipt_updated_at
-  end
-end
